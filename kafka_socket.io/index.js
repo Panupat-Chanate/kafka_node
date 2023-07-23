f48818414b61c7ca0220a5eb272def7ae1dc0242
Panupat-Chanate <panupat.c@psru.ac.th>
2023-07-23 17:50:40 +0700

    wut

--- kafka_socket.io/index.js
+++ kafka_socket.io/index.js
@@ -15,14 +15,6 @@ const io = new Server(server, {
   },
 });
 
-function connectSocket() {
-  return new Promise((resolve, reject) => {
-    io.on("connection", function (socket) {
-      resolve(socket);
-    });
-  });
-}
-
 io.on("connection", (socket) => {
   console.log("socket.id: " + socket.id);
 
@@ -52,30 +44,6 @@ io.on("connection", (socket) => {
     socket.emit("getmessage", { message: socket.id + " leave " + key });
   });
 });
-
-async function run() {
-  console.log("run");
-
-  const socket = await connectSocket();
-
-  if (socket) {
-    console.log("socket.id: " + socket.id);
-
-    socket.emit("say-hi", { message: "Chat connected", id: socket.id });
-
-    socket.on("send-message", ({ key, message }) => {
-      console.log(key, message);
-
-      produce({ from: socket.id, key, message });
-    });
-
-    consume((data) => {
-      socket.emit("get-message", { message: data });
-    });
-  }
-}
-
-// run().catch(console.error);
 
 server.listen(5000, () => {
   console.log("socket.io listening on *:5000");
