8620ad137193ab09a76ef87a8372eb598151bf31
Panupat-Chanate <panupat.c@psru.ac.th>
2023-07-23 23:33:36 +0700

    wut

--- kafka_socket.io/kafka.js
+++ kafka_socket.io/kafka.js
@@ -1,47 +1,24 @@
+"use strict";
 const kafka = require("kafka-node");
-const express = require("express");
-const app = express();
-const http = require("http");
-const server = http.createServer(app);
-const { Server } = require("socket.io");
 
-const Consumer = kafka.Consumer,
-  client = new kafka.KafkaClient({
-    idleConnection: 24 * 60 * 60 * 1000,
-    kafkaHost: "localhost:9092",
-  }),
-  consumer = new Consumer(
-    client,
-    [{ topic: "kafka-panu-topic", partition: 0 }],
-    {
-      autoCommit: true,
-      fetchMaxWaitMs: 1000,
-      fetchMaxBytes: 1024 * 1024,
-      encoding: "utf8",
-      fromOffset: true,
-      keyEncoding: "utf8",
-    }
-  );
-
-server.listen(5000, () => {
-  console.log("socket.io listening on *:5000");
+const Consumer = kafka.Consumer;
+const client = new kafka.KafkaClient({
+  idleConnection: 24 * 60 * 60 * 1000,
+  kafkaHost: "localhost:9092",
 });
 
-const io = new Server(server, {
-  allowEIO3: true,
-  cors: {
-    origin: true,
-    credentials: true,
-  },
-});
-
-io.on("connection", (client) => {
-  console.log("Connected");
-
-  consumer.on("message", function (message) {
-    console.log(message);
+const listen = function (topic, consumerGroup) {
+  const consumer = new Consumer(client, [{ topic: topic }], {
+    autoCommit: true,
+    fetchMaxWaitMs: 1000,
+    fetchMaxBytes: 1024 * 1024,
+    encoding: "utf8",
+    fromOffset: true,
+    keyEncoding: "utf8",
   });
-  client.on("disconnect", () => {
-    console.log("Client disconnected");
-  });
+  return consumer;
+};
+
+listen("kafka-panu-topic", "g1").on("message", function (msg) {
+  console.log("ping", msg);
 });
