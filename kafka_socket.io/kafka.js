f34ad73c5340bfe340d02446c13a806c381ba58c
Panupat-Chanate <panupat.c@psru.ac.th>
2023-07-23 23:04:04 +0700

    wut

--- kafka_socket.io/kafka.js
+++ kafka_socket.io/kafka.js
@@ -5,26 +5,26 @@ const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 
-// const Consumer = kafka.Consumer,
-//   client = new kafka.KafkaClient({
-//     idleConnection: 24 * 60 * 60 * 1000,
-//     kafkaHost: "localhost:9092",
-//   }),
-//   consumer = new Consumer(
-//     client,
-//     [{ topic: "kafka-panu-topic", partition: 0 }],
-//     {
-//       autoCommit: false,
-//     }
-//   );
+const Consumer = kafka.Consumer,
+  client = new kafka.KafkaClient({
+    idleConnection: 24 * 60 * 60 * 1000,
+    kafkaHost: "localhost:9092",
+  }),
+  consumer = new Consumer(
+    client,
+    [{ topic: "kafka-panu-topic", partition: 0 }],
+    {
+      autoCommit: false,
+    }
+  );
 
-// const io = new Server(server, {
-//   allowEIO3: true,
-//   cors: {
-//     origin: true,
-//     credentials: true,
-//   },
-// });
+const io = new Server(server, {
+  allowEIO3: true,
+  cors: {
+    origin: true,
+    credentials: true,
+  },
+});
 
 // io.on("connection", (client) => {
 //   console.log("Connected", client);
