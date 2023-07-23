218cb206c9275e40792b05559da1e03cda23d8ec
Panupat-Chanate <panupat.c@psru.ac.th>
2023-07-23 22:35:26 +0700

    wut

--- kafka_socket.io/kafka.js
+++ kafka_socket.io/kafka.js
@@ -1,13 +1,19 @@
 const kafka = require("kafka-node");
+const Consumer = kafka.Consumer;
 const client = new kafka.KafkaClient({
   idleConnection: 24 * 60 * 60 * 1000,
   kafkaHost: "localhost:9092",
 });
-const consumerGroup = new kafka.ConsumerGroup(client, "kafka-panu-topic");
+const consumer = new Consumer(client, [{ topic: topic, partition: 0 }], {
+  autoCommit: true,
+  fetchMaxWaitMs: 1000,
+  fetchMaxBytes: 1024 * 1024,
+  encoding: "utf8",
+  fromOffset: true,
+});
 
 exports.init = (cb) => {
-  consumerGroup.on("message", function (message) {
-    console.log("Kafka consumer message listner call");
+  consumer.on("message", async function (message) {
     cb(message);
   });
 };
