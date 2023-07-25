d351f325e2e701b200d05c3753b802d05fa51e0b
Panupat-Chanate <panupat.c@psru.ac.th>
2023-07-25 15:25:26 +0700

    wut

--- kafka_socket.io/producer.js
+++ kafka_socket.io/producer.js
@@ -5,14 +5,21 @@ const client = new kafka.KafkaClient({ kafkaHost: "localhost:9092" });
 const producer = new Producer(client, { requireAcks: 0, partitionerType: 2 });
 
 const produce = async ({ topic, key, message }) => {
-  const payload = [
-    {
-      topic: topic,
-      key: key,
-      messages: JSON.stringify(message),
-      timestamp: message.timestamp,
-    },
-  ];
+  // const payload = [
+  //   {
+  //     topic: topic,
+  //     key: key,
+  //     messages: JSON.stringify(message),
+  //     timestamp: message.timestamp,
+  //   },
+  // ];
+
+  const payload = message.map((x) => ({
+    topic: topic,
+    key: key,
+    messages: JSON.stringify(x),
+    timestamp: message.timestamp,
+  }));
 
   producer.send(payload, (error, data) => {
     console.log("producer: " + JSON.stringify(data));
